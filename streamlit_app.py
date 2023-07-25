21271b1a7674bd8e2973116a11eade379d6ee97f
Steven Jerjian <steven.jerjian@gmail.com>
2023-07-25 17:04:13 +0000

    move clus selection inside predict function

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,11 +109,11 @@ clus_for_pred = st.multiselect(
                     "Select clusters for prediction:",
                     options=cluster_labels,
                 )
-clus_for_pred = [f'clus{c}' for c in clus_for_pred]
-#st.write("selected clusters for prediction")
 
 if st.button('Predict'):
     prediction = predict(steps=end_pred, exog=dt_df)
     #pred_df = prediction.pd_dataframe()
     st.write('made prediction')
+    clus_for_pred = [f'clus{c}' for c in clus_for_pred]
+    st.write("selected clusters for prediction")
     #st.line_chart(pred_df, y=clus_for_pred)
